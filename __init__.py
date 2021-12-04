1b6dc2a194adb5b5f6285f84fad8fbbaf8c3395d
Wuyi无疑 <444835641@qq.com>
2021-12-04 19:03:19 +0800

    !!fix!!

--- __init__.py
+++ __init__.py
@@ -243,7 +243,7 @@ async def _(bot: Bot, event: Event):
         mys_data = await GetMysInfo(mysid, cookie)
         mys_data = mys_data[0]
         for i in mys_data['data']['list']:
-            if i['data'][0]['name'] != '活跃天数':
+            if i['game_id'] != 2:
                 mys_data['data']['list'].remove(i)
         uid = mys_data['data']['list'][0]['game_role_id']
         await cookiesDB(uid, cookie, event.sender.user_id)

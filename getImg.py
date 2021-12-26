823e9981143b6c7e2291b2dec4e27e286ef21cb3
Wuyi无疑 <444835641@qq.com>
2021-12-26 22:18:06 +0800

    修复：活动列表可能导致的bug

--- getImg.py
+++ getImg.py
@@ -1293,7 +1293,13 @@ async def draw_event_pic():
             elif k["subtitle"] in i["name"]:
                 k["act_begin_time"] = i["act_begin_time"]
                 k["act_end_time"] = i["act_end_time"]
-
+            else:
+                k["act_begin_time"] = "{}-{}-{} {}".format(k["start_time"].split()[0].split("-")[0], 
+                                                           k["start_time"].split()[0].split("-")[1],
+                                                           str(int(k["start_time"].split()[0].split("-")[2])+2),
+                                                           "10:00:00(?)")
+                k["act_end_time"] = k["end_time"]
+                
         if "冒险助力礼包" in k["title"] or "纪行" in k["title"]:
             continue
         #if "角色试用" in k["title"] or "传说任务" in k["title"]:

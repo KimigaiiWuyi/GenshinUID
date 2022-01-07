e59d4ed1e51fdeb3081c46748a93655e5629cbb6
student_2333 <lgc2333@126.com>
2022-01-07 11:09:50 +0800

    edit

--- __init__.py
+++ __init__.py
@@ -276,7 +276,8 @@ async def enemies_wiki(name):
     raw_data = await GetEnemiesInfo(name)
     reward = ""
     for i in raw_data["rewardpreview"]:
-        reward += i["name"] + "：" + parse_percent(i["count"]) if "count" in i.keys() else i["name"] + "：" + "可能"
+        reward += i["name"] +("("+"★"*int(i["rarity"])+")" if "rarity" in i.keys() else "") + "：" 
+        reward += parse_percent(i["count"]) if "count" in i.keys() else "可能"
         reward += "\n"
     im = "【{}】\n——{}——\n类型：{}\n信息：{}\n掉落物：\n{}".format(raw_data["name"],raw_data["specialname"],
                                                     raw_data["category"],raw_data["description"],reward)

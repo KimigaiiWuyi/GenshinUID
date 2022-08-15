efbfea22e1e07c69f70dd03ec7535e4cdc57e6b0
Wuyi无疑 <444835641@qq.com>
2022-08-15 13:32:21 +0800

    :alien: 尝试通过重试修复`签到`、`全部重签`

--- genshinuid_signin/sign.py
+++ genshinuid_signin/sign.py
@@ -19,24 +19,45 @@ group_msg_list = {}
 async def sign_in(uid) -> str:
     logger.info(f'[签到] {uid} 开始执行签到')
     sign_info = await get_sign_info(uid)
+    sign_info = sign_info['data']
+    if sign_info['is_sign']:
+        logger.info(f'[签到] {uid} 该用户今日已签到,跳过...')
+        return '该用户今日已签到,跳过...'
     sign_data = await mihoyo_bbs_sign(uid=uid)
-    print(sign_data)
     logger.debug(sign_data)
     if sign_data:
-        sign_info = sign_info['data']
+        if 'risk_code' in sign_data['data']:
+            if sign_data['data']['risk_code'] == 375:
+                for _ in range(3):
+                    sign_data = await mihoyo_bbs_sign(uid=uid)
+                    if 'risk_code' in sign_data['data']:
+                        if sign_data['data']['risk_code'] == 0:
+                            logger.info(f'[签到] {uid} 该用户成功跳过校验码！')
+                            break
+                        else:
+                            logger.info(f'[签到] {uid} 该用户出现校验码，等待重试...')
+                            await asyncio.sleep(10 + random.randint(1, 15))
+                    else:
+                        logger.warning(f'[签到] 超过请求阈值...')
+                        break
+                else:
+                    logger.warning(f'[签到] {uid} 该用户出现校验码，校验失败！')
+                    return '签到失败...出现验证码!请过段时间[全部重签]或者手动至米游社进行签到！'
         sign_list = await get_sign_list()
         status = sign_data['message']
-        getitem = sign_list['data']['awards'][
-            int(sign_info['total_sign_day']) - 1
-        ]['name']
-        getnum = sign_list['data']['awards'][
-            int(sign_info['total_sign_day']) - 1
-        ]['cnt']
+        getitem = sign_list['data']['awards'][int(sign_info['total_sign_day']) - 1][
+            'name'
+        ]
+        getnum = sign_list['data']['awards'][int(sign_info['total_sign_day']) - 1][
+            'cnt'
+        ]
         get_im = f'本次签到获得{getitem}x{getnum}'
-        if status == 'OK' and sign_info['is_sign'] == True:
+        new_sign_info = await get_sign_info(uid)
+        new_sign_info = sign_info['data']
+        if new_sign_info['is_sign'] == True:
             mes_im = '签到成功'
         else:
-            mes_im = status
+            mes_im = f'签到失败, 状态为:{status}'
         sign_missed = sign_info['sign_cnt_missed']
         im = mes_im + '!' + '\n' + get_im + '\n' + f'本月漏签次数：{sign_missed}'
         logger.info(f'[签到] {uid} 签到完成, 结果: {mes_im}, 漏签次数: {sign_missed}')
@@ -66,7 +87,7 @@ async def single_daily_sign(uid: str, gid: str, qid: str):
         # 检查是否开启简洁签到
         if await config_check('SignReportSimple'):
             # 如果失败, 则添加到推送列表
-            if im == '签到失败' or im.startswith('网络有点忙'):
+            if im.startswith('签到失败') or im.startswith('网络有点忙'):
                 # 不用MessageSegment.at(row[2])，因为不方便移植
                 message = f'[CQ:at,qq={qid}] {im}'
                 group_msg_list[gid]['failed'] += 1
@@ -100,9 +121,9 @@ async def daily_sign():
     tasks = []
     for row in c_data:
         tasks.append(single_daily_sign(row['UID'], row['StatusB'], row['QID']))
-        if len(tasks) >= 1:
+        if len(tasks) >= 3:
             await asyncio.gather(*tasks)
-            delay = 30 + random.randint(2, 8)
+            delay = 20 + random.randint(1, 15)
             logger.info(f'[签到] 已签到{len(tasks)}个用户, 等待{delay}秒进行下一次签到')
             tasks.clear()
             await asyncio.sleep(delay)

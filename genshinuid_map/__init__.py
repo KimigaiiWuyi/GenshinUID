5951bd6262db2520ade0c0107ffc35ab7340057c
student_2333 <lgc2333@126.com>
2022-08-16 03:17:22 +0800

    优化资源点查询 优化地图名称提示 优化查询语句匹配

--- genshinuid_map/__init__.py
+++ genshinuid_map/__init__.py
@@ -1,29 +1,38 @@
-from pathlib import Path
+from typing import Dict
 
-from ..all_import import *  # noqa
-from .genshinmap.models import MapID
+from nonebot.params import RegexDict
+
 from .create_genshinmap import create_genshin_map
 from .draw_genshinmap_card import draw_genshin_map
+from .genshinmap.models import MapID
+from ..all_import import *  # noqa
 
 create_map = on_command('生成地图', block=True)
 change_map = on_command('切换地图', block=True)
-find_map = on_regex(r'^(.*)(在哪里|在哪|哪里有|在哪儿)$', priority=2)
+find_map = on_regex(r'^(?P<name>.*)(在哪里|在哪|哪里有|哪儿有|哪有|在哪儿)$', priority=2)
+find_map2 = on_regex(r'^(哪里有|哪儿有|哪有)(?P<name>.*)$', priority=2)
 
 MAP_DATA = Path(__file__).parent / 'map_data'
 MAP_ID_LIST = [
-    MapID.teyvat,
-    MapID.chasm,
-    MapID.enkanomiya,
-    MapID.golden_apple_archipelago,
+    MapID.teyvat,  # 提瓦特
+    MapID.chasm,  # 层岩巨渊
+    MapID.enkanomiya,  # 渊下宫
+    MapID.golden_apple_archipelago,  # 金苹果群岛
 ]
+MAP_CHN_NAME = {
+    MapID.teyvat: '提瓦特',
+    MapID.chasm: '层岩巨渊',
+    MapID.enkanomiya: '渊下宫',
+    MapID.golden_apple_archipelago: '金苹果群岛',
+}
 
 
 @change_map.handle()
 @handle_exception('切换地图')
 async def send_change_map_msg(
-    event: Union[GroupMessageEvent, PrivateMessageEvent],
-    matcher: Matcher,
-    args: Message = CommandArg(),
+        event: Union[GroupMessageEvent, PrivateMessageEvent],
+        matcher: Matcher,
+        args: Message = CommandArg(),
 ):
     if args:
         return
@@ -33,16 +42,18 @@ async def send_change_map_msg(
     logger.info('[切换地图]正在执行...')
     MAP_ID_LIST.append(MAP_ID_LIST[0])
     MAP_ID_LIST.pop(0)
-    logger.info(f'[切换地图]当前地图为{MAP_ID_LIST[0]}')
-    await matcher.finish(f'切换到{MAP_ID_LIST[0].name}地图')
+    current = MAP_ID_LIST[0]
+    chn = MAP_CHN_NAME.get(current)
+    logger.info(f'[切换地图]当前地图为{chn}("{current.name}", {current.value})')
+    await matcher.finish(f'切换到{chn}地图')
 
 
 @create_map.handle()
 @handle_exception('生成地图')
 async def send_create_map_msg(
-    event: Union[GroupMessageEvent, PrivateMessageEvent],
-    matcher: Matcher,
-    args: Message = CommandArg(),
+        event: Union[GroupMessageEvent, PrivateMessageEvent],
+        matcher: Matcher,
+        args: Message = CommandArg(),
 ):
     if args:
         return
@@ -56,21 +67,24 @@ async def send_create_map_msg(
 
 
 @find_map.handle()
+@find_map2.handle()
 @handle_exception('查找资源点')
 async def send_find_map_msg(
-    matcher: Matcher,
-    args: Tuple[Any, ...] = RegexGroup(),
+        matcher: Matcher,
+        args: Dict[str, Any] = RegexDict()  # noqa
 ):
     logger.info(f'[查找资源点]正在执行...当前地图为{MAP_ID_LIST[0].name}')
     logger.info('[查找资源点]参数: {}'.format(args))
-    resource_temp_path = MAP_DATA / f'{MAP_ID_LIST[0].name}_{args[0]}.jpg'
+
+    args = args.get('name')
+    resource_temp_path = MAP_DATA / f'{MAP_ID_LIST[0].name}_{args}.jpg'
     if resource_temp_path.exists():
-        logger.info(f'本地已有{MAP_ID_LIST[0].name}_{args[0]}的资源点,直接发送...')
+        logger.info(f'本地已有{MAP_ID_LIST[0].name}_{args}的资源点,直接发送...')
         await matcher.finish(MessageSegment.image(resource_temp_path))
     else:
-        await matcher.send(f'正在查找{args[0]},可能需要比较久的时间...')
+        await matcher.send(f'正在查找{args},可能需要比较久的时间...\n当前地图：{MAP_CHN_NAME.get(MAP_ID_LIST[0])}')
         logger.info(f'本地未缓存,正在渲染...')
-        im = await draw_genshin_map(MAP_ID_LIST[0], args[0])
+        im = await draw_genshin_map(MAP_ID_LIST[0], args)
     if isinstance(im, str):
         await matcher.finish(im)
     elif isinstance(im, bytes):

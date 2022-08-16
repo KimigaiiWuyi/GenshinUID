ab8cfe3290f993825ea749076012a5d86a9edb77
student_2333 <lgc2333@126.com>
2022-08-16 20:42:14 +0800

    :zap: 优化 (#296)
    * 优化资源下载
    
    * 优化资源点查询
    优化地图名称提示
    优化查询语句匹配

---
+++ genshinuid_resource/__init__.py
@@ -0,0 +1,28 @@
+import threading
+
+from ..all_import import *  # noqa: F403, F401
+from ..utils.download_resource.download_all_resource import (
+    download_all_resource,
+)
+
+download_resource = on_command('下载全部资源')
+
+
+@download_resource.handle()
+@handle_exception('下载全部资源', '资源下载错误')
+async def send_download_resource_msg(
+        matcher: Matcher, args: Message = CommandArg()
+):
+    if args:
+        return
+    await matcher.send('正在开始下载~可能需要较久的时间!')
+    im = await download_all_resource()
+    await matcher.finish(im)
+
+
+async def startup():
+    logger.info('[资源文件下载] 正在检查与下载缺失的资源文件，可能需要较长时间，请稍等')
+    logger.info(f'[资源文件下载] {await download_all_resource()}')
+
+
+threading.Thread(target=lambda: asyncio.run(startup()), daemon=True).start()
--- utils/download_resource/download_all_resource.py
+++ utils/download_resource/download_all_resource.py
@@ -1,12 +1,13 @@
+import asyncio
+
+from .download_from_fandom import download_by_fandom, get_char_url_list
 from .download_from_minigg import download_by_minigg
-from .download_from_fandom import get_char_url_list, download_by_fandom
 
 
 async def download_all_resource():
     lst = await get_char_url_list()
-    faild_str = ''
-    faild_str += await download_by_fandom(lst)
-    faild_str += await download_by_minigg()
-    if faild_str:
-        return faild_str
+    ret = await asyncio.gather(download_by_fandom(lst), download_by_minigg())
+    ret = [str(x) for x in ret if x]
+    if ret:
+        return '\n'.join(ret)
     return '全部资源下载完成!'

fad0f6521dd1103c35d928273c9c08391102b3c1
Ice-Cirno <438971718@qq.com>
2022-09-07 21:33:00 +0800

    🐛 fix some obvious error in hoshino branch (#321)
    * use service logger
    
    * fix requirements
    
    * add aliases of gs帮助
    
    * fix import error in genshinuid_mys

--- genshinuid_mys/__init__.py
+++ genshinuid_mys/__init__.py
@@ -1,11 +1,11 @@
 import asyncio
 
-from all_import import *
+from ..all_import import *
 
 from .get_mys_data import get_region_task, get_task_detail
 
 
-@sv.on_regex('(原神任务|任务|任务详情|任务攻略)( )?([\u4e00-\u9fa5]+)( )?')
+@sv.on_rex('(原神任务|任务|任务详情|任务攻略)( )?([\u4e00-\u9fa5]+)( )?')
 async def send_task_adv(bot: HoshinoBot, ev: CQEvent):
     args = ev['match'].groups()
     if str(args[2]) in ['须弥', '层岩', '海岛']:

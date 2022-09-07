ec95a3934d01dd2df1e4b0747f44a53e078f365a
KimgiaiiWuyi <444835641@qq.com>
2022-09-08 02:32:22 +0800

    ✨ `gs帮助`现在可用

--- genshinuid_help/__init__.py
+++ genshinuid_help/__init__.py
@@ -1,3 +1,7 @@
+import asyncio
+import threading
+
+from .draw_help_card import draw_help_img
 from ..all_import import *  # noqa: F403,F401
 
 HELP_IMG = Path(__file__).parent / 'help.png'
@@ -8,3 +12,8 @@ async def send_guide_pic(bot, ev):
     img = await convert_img(HELP_IMG)
     logger.info('获得gs帮助图片成功！')
     await bot.send(ev, img)
+
+
+threading.Thread(
+    target=lambda: asyncio.run(draw_help_img()), daemon=True
+).start()

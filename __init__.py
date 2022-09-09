82f7b05a6993209dd07c290eea460cc872c95b6b
MingxuanGame <MingxuanGame@outlook.com>
2022-09-09 22:30:11 +0800

    :art: change the warning colors

--- __init__.py
+++ __init__.py
@@ -7,11 +7,11 @@ gsuid_path = Path(__file__).parent
 WARNING = f"""
 不推荐直接加载 GenshinUID 仓库文件夹，请使用 pip 安装此包，然后使用加载插件的方式加载
 安装命令：
-使用 pip：<y>pip install {gsuid_path}</y>
+使用 pip：<magenta>pip install {gsuid_path}</magenta>
 {
     (
-        "使用 nb-cli：<y>nb plugin install git+https://github.com/"
-        "KimigaiiWuyi/GenshinUID.git@nonebot2-beta1#egg=GenshinUID</y>"
+        "使用 nb-cli：<magenta>nb plugin install git+https://github.com/"
+        "KimigaiiWuyi/GenshinUID.git@nonebot2-beta1#egg=GenshinUID</magenta>"
     )
 }
 加载插件：<blue>https://v2.nonebot.dev/docs/tutorial/plugin/load-plugin</blue>

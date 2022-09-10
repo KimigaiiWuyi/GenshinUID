29f7739a1ba64e81c966e0a1578ee71767b1b31c
student_2333 <lgc2333@126.com>
2022-09-10 22:26:22 +0800

    fix

--- genshinuid_map/__init__.py
+++ genshinuid_map/__init__.py
@@ -56,9 +56,7 @@ async def send_find_map_msg(bot: HoshinoBot, ev: CQEvent):
     logger.info(f'[查找资源点]正在执行...当前地图为{MAP_ID_LIST[0].name}')
     logger.info('[查找资源点]参数: {}'.format(args))
 
-    if args[0]:
-        args = args[0]
-    else:
+    if not args:
         return
 
     im = ''

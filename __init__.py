27b343a95e5476a8675a4ad33c7f4bf008736a3d
MingxuanGame <MingxuanGame@outlook.com>
2022-09-10 18:00:01 +0800

    :bug: auto move: delete utils

--- __init__.py
+++ __init__.py
@@ -14,7 +14,7 @@ def copy_and_delete_files():
         logger.info("检测到未迁移的文件夹，执行迁移")
     is_first = True
     for path in dir_.iterdir():
-        if path.name.startswith("genshinuid_"):
+        if path.name.startswith("genshinuid_") or path.name == "utils":
             if is_first:
                 logger.info("检测到残留文件夹，执行删除")
                 is_first = False
@@ -45,7 +45,7 @@ def copy_and_delete_files():
                     src, dst, exc = tup
                     failed_src.append(Path(src).absolute())
                     logger.warning(f"复制文件夹 {src} -> {dst} 失败：{exc}")
-                now_src = [i.absolute() for i in path.iterdir()]
+                now_src = {i.absolute() for i in path.iterdir()}
                 successful_src = filter(lambda x: x not in now_src, failed_src)
                 for i in successful_src:
                     try:

7d98e374ee12d4c441644159fa59a78d81af63e2
KimgiaiiWuyi <444835641@qq.com>
2022-09-17 00:17:33 +0800

    🎨 分离`添加`时发送的消息类型

--- GenshinUID/genshinuid_user/add_ck.py
+++ GenshinUID/genshinuid_user/add_ck.py
@@ -12,13 +12,35 @@ from ..utils.mhy_api.get_mhy_data import (
 pic_path = Path(__file__).parent / 'pic'
 
 
-async def deal_ck(mes, qid):
+async def deal_ck(mes, qid, mode: str = 'PIC'):
+    im = await _deal_ck(mes, qid)
+    if mode == 'PIC':
+        im = await _deal_ck_to_pic(im)
+    return im
+
+
+async def _deal_ck_to_pic(im) -> bytes:
+    ok_num = im.count('成功')
+    if ok_num < 1:
+        status_pic = pic_path / 'ck_no.png'
+    elif ok_num < 2:
+        status_pic = pic_path / 'ck_ok.png'
+    else:
+        status_pic = pic_path / 'all_ok.png'
+    with open(status_pic, 'rb') as f:
+        im = f.read()
+    return im
+
+
+async def _deal_ck(mes, qid) -> str:
     simp_dict = SimpleCookie(mes)
+    '''
     uid = await select_db(qid, 'uid')
     if isinstance(uid, str):
         pass
     else:
         return '该用户没有绑定过UID噢~'
+    '''
     im_list = []
     is_add_stoken = False
     app_cookie, stoken = '', ''
@@ -80,13 +102,4 @@ async def deal_ck(mes, qid):
     )
     im_list.append('你可以使用命令【绑定信息】检查你的账号绑定情况！')
     im = '\n'.join(im_list)
-    ok_num = im.count('成功')
-    if ok_num < 1:
-        status_pic = pic_path / 'ck_no.png'
-    elif ok_num < 2:
-        status_pic = pic_path / 'ck_ok.png'
-    else:
-        status_pic = pic_path / 'all_ok.png'
-    with open(status_pic, 'rb') as f:
-        im = f.read()
     return im

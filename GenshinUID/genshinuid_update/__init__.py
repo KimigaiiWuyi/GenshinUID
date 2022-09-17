d1312c5ecc4e1698cac5eb05a4cb3ea24a208217
KimgiaiiWuyi <444835641@qq.com>
2022-09-17 21:32:20 +0800

    ✨ `gs重启`

--- GenshinUID/genshinuid_update/__init__.py
+++ GenshinUID/genshinuid_update/__init__.py
@@ -1,29 +1,69 @@
-from typing import Any, Tuple, Union
+from typing import Any, Tuple
 
-from nonebot import on_regex
 from nonebot.log import logger
 from nonebot.matcher import Matcher
 from nonebot.params import RegexGroup
 from nonebot.permission import SUPERUSER
-from nonebot.adapters.onebot.v11 import (
-    Bot,
-    GroupMessageEvent,
-    PrivateMessageEvent,
-)
+from nonebot.adapters.onebot.v11 import Bot, MessageEvent
+from nonebot import get_bot, on_regex, get_driver, on_command
 
 from .update import update_genshinuid
+from ..utils.nonebot2.rule import FullCommand
+from .restart import restart_message, restart_genshinuid
 
-# gs_restart = on_command('gs重启', rule=FullCommand())
+gs_restart = on_command('gs重启', rule=FullCommand())
 gs_update = on_regex(
     r'^(gs)(强行)?(强制)?(更新)$',
     block=True,
 )
 
 
+@get_driver().on_bot_connect
+async def _():
+    logger.info('检查遗留信息...')
+    bot = get_bot()
+    update_log = await restart_message()
+    if update_log == {}:
+        return
+    if update_log['send_type'] == 'group':
+        await bot.call_api(
+            api='send_group_msg',
+            group_id=update_log['send_to'],
+            message=update_log['msg'],
+        )
+    else:
+        await bot.call_api(
+            api='send_private_msg',
+            user_id=update_log['send_to'],
+            message=update_log['msg'],
+        )
+    logger.info('遗留信息检查完毕!')
+
+
+@gs_restart.handle()
+async def send_restart_msg(
+    bot: Bot,
+    event: MessageEvent,
+    matcher: Matcher,
+):
+    if not await SUPERUSER(bot, event):
+        return
+    logger.warning('开始执行[重启]')
+    qid = event.user_id
+    if len(event.get_session_id().split('_')) == 3:
+        send_id = event.get_session_id().split('_')[1]
+        send_type = 'group'
+    else:
+        send_id = qid
+        send_type = 'private'
+    await matcher.send('正在执行[gs重启]...')
+    await restart_genshinuid(send_type, str(send_id))
+
+
 @gs_update.handle()
 async def send_update_msg(
     bot: Bot,
-    event: Union[GroupMessageEvent, PrivateMessageEvent],
+    event: MessageEvent,
     matcher: Matcher,
     args: Tuple[Any, ...] = RegexGroup(),
 ):

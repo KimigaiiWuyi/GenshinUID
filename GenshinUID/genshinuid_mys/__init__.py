3cb7a5c34bec12f60d4491ab4322f4f515cad0b3
KimgiaiiWuyi <444835641@qq.com>
2022-10-01 21:58:44 +0800

    ✨ 新增`抽表情`(#344)

--- GenshinUID/genshinuid_mys/__init__.py
+++ GenshinUID/genshinuid_mys/__init__.py
@@ -1,18 +1,23 @@
 import asyncio
 from typing import Any, Tuple
 
-from nonebot import on_regex
+from nonebot.log import logger
 from nonebot.matcher import Matcher
 from nonebot.params import RegexGroup
+from nonebot import on_regex, on_command
 from nonebot.adapters.qqguild import Bot, MessageEvent
 
 from ..config import priority
+from .get_meme_card import get_meme_img
+from ..utils.nonebot2.rule import FullCommand
+from ..utils.nonebot2.send import local_image
 from .get_mys_data import get_region_task, get_task_detail
 from ..utils.exception.handle_exception import handle_exception
 
 get_task_adv = on_regex(
     '(原神任务|任务|任务详情|任务攻略)( )?([\u4e00-\u9fa5]+)( )?', priority=priority
 )
+get_meme = on_command('抽表情', priority=priority, rule=FullCommand())
 
 
 @get_task_adv.handle()
@@ -37,3 +42,11 @@ async def send_task_adv(
     else:
         im = await get_task_detail(str(args[2]))
         await matcher.finish(im)
+
+
+@get_meme.handle()
+@handle_exception('抽表情')
+async def send_primogems_data(matcher: Matcher):
+    logger.info('开始执行[抽表情]')
+    img = await get_meme_img()
+    await matcher.finish(local_image(img))

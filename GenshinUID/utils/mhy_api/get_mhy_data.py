461215a2d5d0f410aabaf04bd83ad71ae70ca08f
KimgiaiiWuyi <444835641@qq.com>
2022-10-06 23:15:02 +0800

    ✨ 新增`gs开启跳过无感验证`

--- GenshinUID/utils/mhy_api/get_mhy_data.py
+++ GenshinUID/utils/mhy_api/get_mhy_data.py
@@ -1,4 +1,5 @@
 import copy
+import json
 import time
 import random
 import asyncio
@@ -16,6 +17,7 @@ from ..mhy_api.mhy_api_tools import (
 )
 from ..mhy_api.mhy_api import (
     SIGN_URL,
+    GT_TEST_URL,
     SIGN_INFO_URL,
     SIGN_LIST_URL,
     DAILY_NOTE_URL,
@@ -199,6 +201,28 @@ async def get_daily_data(uid: str, server_id: str = 'cn_gf01') -> dict:
     return data
 
 
+async def get_validate(gt: str, challenge: str) -> str:
+    HEADER = {}
+    HEADER['User-Agent'] = (
+        'Mozilla/5.0 (Linux; Android 10; MIX 2 Build/QKQ1.190825.002; wv) '
+        'AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 '
+        'Chrome/83.0.4103.101 Mobile Safari/537.36 miHoYoBBS/2.35.2'
+    )
+    HEADER['Accept'] = '*/*'
+    HEADER['Accept-Language'] = 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7'
+    HEADER['Referer'] = 'https://webstatic.mihoyo.com/'
+    HEADER['X-Requested-With'] = 'com.mihoyo.hyperion'
+    validate = ''
+    data = await _mhy_request(GT_TEST_URL.format(gt, challenge), 'GET', HEADER)
+    if (
+        data
+        and 'success' in data['status']
+        and 'success' in data['data']['result']
+    ):
+        validate = data['data']['validate']
+    return validate
+
+
 async def get_sign_list() -> dict:
     data = await _mhy_request(
         url=SIGN_LIST_URL,
@@ -223,22 +247,15 @@ async def get_sign_info(uid, server_id='cn_gf01') -> dict:
     return data
 
 
-async def mihoyo_bbs_sign(uid, ua=None, server_id='cn_gf01') -> dict:
+async def mihoyo_bbs_sign(uid, Header={}, server_id='cn_gf01') -> dict:
     if uid[0] == '5':
         server_id = 'cn_qd01'
     HEADER = copy.deepcopy(_HEADER)
-    if ua == 'iphone':
-        HEADER['User-Agent'] = (
-            'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X)'
-            'AppleWebKit/605.1.15 (KHTML, like Gecko) '
-            'miHoYoBBS/miHoYoBBS/2.35.2'
-        )
-    else:
-        HEADER['User_Agent'] = (
-            'Mozilla/5.0 (Linux; Android 10; MIX 2 Build/QKQ1.190825.002; wv) '
-            'AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 '
-            'Chrome/83.0.4103.101 Mobile Safari/537.36 miHoYoBBS/2.35.2'
-        )
+    HEADER['User_Agent'] = (
+        'Mozilla/5.0 (Linux; Android 10; MIX 2 Build/QKQ1.190825.002; wv) '
+        'AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 '
+        'Chrome/83.0.4103.101 Mobile Safari/537.36 miHoYoBBS/2.35.2'
+    )
     HEADER['Cookie'] = await owner_cookies(uid)
     HEADER['x-rpc-device_id'] = random_hex(32)
     HEADER['x-rpc-app_version'] = '2.35.2'
@@ -250,6 +267,9 @@ async def mihoyo_bbs_sign(uid, ua=None, server_id='cn_gf01') -> dict:
         '?bbs_auth_required=true&act_id=e202009291139501&utm_source=bbs'
         '&utm_medium=mys&utm_campaign=icon'
     )
+    print(Header)
+    HEADER.update(Header)
+    print(HEADER)
     data = await _mhy_request(
         url=SIGN_URL,
         method='POST',
@@ -425,6 +445,10 @@ async def _mhy_request(
         req = await sess.request(
             method, url=url, headers=header, params=params, json=data
         )
+        text_data = await req.text()
+        if text_data.startswith('('):
+            text_data = json.loads(text_data.replace("(", "").replace(")", ""))
+            return text_data
         return await req.json()
     except Exception:
         logger.exception(f'访问{url}失败！')

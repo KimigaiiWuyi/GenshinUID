4c68c1bb45e59e3c5a2920276ffdfe20aeef6548
KimgiaiiWuyi <444835641@qq.com>
2022-10-12 23:59:02 +0800

    ✨ 重做的`毕业度统计`

--- GenshinUID/genshinuid_enka/__init__.py
+++ GenshinUID/genshinuid_enka/__init__.py
@@ -2,7 +2,6 @@ import re
 import json
 import random
 import asyncio
-from typing import Union
 
 from nonebot.log import logger
 from nonebot.matcher import Matcher
@@ -12,14 +11,15 @@ from nonebot.params import Depends, CommandArg
 from nonebot.adapters.qqguild import Bot, Message, MessageEvent
 
 from ..config import priority
+from .draw_char_card import draw_char_img
 from ..genshinuid_meta import register_menu
 from ..utils.nonebot2.send import local_image
+from .draw_char_rank import draw_cahrcard_list
 from ..utils.message.cast_type import cast_to_int
 from ..utils.enka_api.get_enka_data import switch_api
 from ..utils.enka_api.enka_to_data import enka_to_data
 from ..utils.message.get_image_and_at import ImageAndAt
 from ..utils.message.error_reply import UID_HINT, CHAR_HINT
-from .draw_char_card import draw_char_img, draw_cahrcard_list
 from ..utils.alias.alias_to_char_name import alias_to_char_name
 from ..utils.download_resource.RESOURCE_PATH import PLAYER_PATH
 from ..utils.exception.handle_exception import handle_exception
@@ -279,20 +279,21 @@ async def send_charcard_list(
     args: Message = CommandArg(),
     custom: ImageAndAt = Depends(),
 ):
-
-    message = args.extract_plain_text().strip().replace(' ', '')
-    limit = re.findall(r'\d+', message)  # str
-    if len(limit) >= 1:
-        limit = int(limit[0])
-    else:
-        limit = 24
+    raw_mes = args.extract_plain_text().strip()
+    qid = str(cast_to_int(event.author))
     at = custom.get_first_at()
     if at:
-        uid = await select_db(at, mode='uid')
-        message = message.replace(str(at), '')
+        qid = at
+
+    # 获取uid
+    uid = re.findall(r'\d+', raw_mes)
+    if uid:
+        uid = uid[0]
     else:
-        uid = await select_db(str(cast_to_int(event.author)), mode='uid')
-    im = await draw_cahrcard_list(str(uid), limit)
+        uid = await select_db(qid, mode='uid')
+        uid = str(uid)
+
+    im = await draw_cahrcard_list(str(uid), qid)
 
     logger.info(f'UID{uid}获取角色数据成功！')
     if isinstance(im, bytes):

9ac01864c5c5a12749fafc94b170dd031e3c916f
KimgiaiiWuyi <444835641@qq.com>
2022-10-14 22:26:12 +0800

    ✨ 重做的`活动列表`和新增`卡池列表`

--- GenshinUID/genshinuid_eventlist/__init__.py
+++ GenshinUID/genshinuid_eventlist/__init__.py
@@ -5,16 +5,17 @@ from nonebot.adapters.onebot.v11 import MessageSegment
 from ..config import priority
 from ..genshinuid_meta import register_menu
 from ..utils.nonebot2.rule import FullCommand
-from .draw_event_img import IMG_PATH, save_draw_event_img
+from .draw_event_img import get_event_img, get_all_event_img
 from ..utils.exception.handle_exception import handle_exception
 
 get_event = on_command('活动列表', priority=priority, rule=FullCommand())
+get_gacha = on_command('卡池列表', priority=priority, rule=FullCommand())
 scheduler = require('nonebot_plugin_apscheduler').scheduler
 
 
 @scheduler.scheduled_job('cron', hour='2')
 async def draw_event():
-    await save_draw_event_img()
+    await get_all_event_img()
 
 
 @get_event.handle()
@@ -26,11 +27,18 @@ async def draw_event():
     detail_des=('指令：' '<ft color=(238,120,0)>活动列表</ft>\n' ' \n' '查询当前版本活动日程表'),
 )
 async def send_events(matcher: Matcher):
-    while True:
-        if IMG_PATH.exists():
-            with open(IMG_PATH, 'rb') as f:
-                im = MessageSegment.image(f.read())
-            break
-        else:
-            await save_draw_event_img()
-    await matcher.finish(im)
+    img = await get_event_img('EVENT')
+    await matcher.finish(MessageSegment.image(img))
+
+
+@get_gacha.handle()
+@handle_exception('活动')
+@register_menu(
+    '卡池列表',
+    '卡池列表',
+    '查询当前版本卡池列表',
+    detail_des=('指令：' '<ft color=(238,120,0)>卡池列表</ft>\n' ' \n' '查询当前版本卡池列表'),
+)
+async def send_gachas(matcher: Matcher):
+    img = await get_event_img('GACHA')
+    await matcher.finish(MessageSegment.image(img))

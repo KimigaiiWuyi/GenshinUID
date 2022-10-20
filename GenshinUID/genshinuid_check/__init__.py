0936856218411a529e329cc37d4e98c4c73f572e
KimgiaiiWuyi <444835641@qq.com>
2022-10-21 00:42:34 +0800

    ✨ 新增`gs清除缓存`(如查询相关异常可尝试)

--- GenshinUID/genshinuid_check/__init__.py
+++ GenshinUID/genshinuid_check/__init__.py
@@ -3,7 +3,8 @@ import asyncio
 
 from nonebot.matcher import Matcher
 from nonebot import require, on_command
-from nonebot.adapters.onebot.v11 import Bot
+from nonebot.permission import SUPERUSER
+from nonebot.adapters.qqguild import Bot, MessageEvent
 
 from .backup_data import data_backup
 from ..genshinuid_meta import register_menu
@@ -11,6 +12,7 @@ from ..utils.nonebot2.rule import FullCommand
 from ..utils.exception.handle_exception import handle_exception
 from ..utils.db_operation.db_cache_and_check import check_db, check_stoken_db
 
+backup = on_command('gs清除缓存', rule=FullCommand())
 check = on_command('校验全部Cookies', rule=FullCommand())
 check_stoken = on_command('校验全部Stoken', rule=FullCommand())
 
@@ -20,6 +22,19 @@ backup_scheduler = require('nonebot_plugin_apscheduler').scheduler
 @backup_scheduler.scheduled_job('cron', hour=0)
 async def daily_refresh_charData():
     await data_backup()
+
+
+@backup.handle()
+@handle_exception('清除缓存', '清除缓存错误')
+async def send_backup_msg(
+    bot: Bot,
+    event: MessageEvent,
+    matcher: Matcher,
+):
+    if not await SUPERUSER(bot, event):
+        return
+    await data_backup()
+    await matcher.finish(f'操作成功完成!')
 
 
 # 群聊内 校验Cookies 是否正常的功能，不正常自动删掉

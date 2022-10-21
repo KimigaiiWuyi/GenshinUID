8be890316ddce64cf7897e42781c0c694aa17bd8
KimgiaiiWuyi <444835641@qq.com>
2022-10-22 00:12:32 +0800

    ✨ 新增`更新记录`

--- GenshinUID/genshinuid_update/__init__.py
+++ GenshinUID/genshinuid_update/__init__.py
@@ -1,8 +1,9 @@
 from nonebot import get_bot, on_startup
 
-from .update import update_genshinuid
 from ..all_import import *  # noqa: F403,F401
+from .draw_update_log import draw_update_log_img
 from .restart import restart_message, restart_genshinuid
+from ..utils.draw_image_tools.send_image_tool import convert_img
 
 
 @on_startup
@@ -48,6 +49,19 @@ async def send_restart_msg(bot: HoshinoBot, ev: CQEvent):
     await restart_genshinuid(send_type, str(send_id))
 
 
+@sv.on_fullmatch('更新记录')
+async def send_updatelog_msg(bot: HoshinoBot, ev: CQEvent):
+    im = await draw_update_log_img(is_update=False)
+    logger.info('正在执行[更新记录]...')
+    if isinstance(im, str):
+        await bot.send(ev, im)
+    elif isinstance(im, bytes):
+        im = await convert_img(im)
+        await bot.send(ev, im)
+    else:
+        await bot.send(ev, '发生了未知错误,请联系管理员检查后台输出!')
+
+
 @sv.on_rex(r'^(gs)(强行)?(强制)?(更新)$')
 async def send_update_msg(bot: HoshinoBot, ev: CQEvent):
     if ev.sender:
@@ -66,5 +80,11 @@ async def send_update_msg(bot: HoshinoBot, ev: CQEvent):
         level -= 1
     logger.info(f'[gs更新] 更新等级为{level}')
     await bot.send(ev, f'开始执行[gs更新], 执行等级为{level}')
-    im = await update_genshinuid(level)
-    await bot.send(ev, im)
+    im = await draw_update_log_img(level)
+    if isinstance(im, str):
+        await bot.send(ev, im)
+    elif isinstance(im, bytes):
+        im = await convert_img(im)
+        await bot.send(ev, im)
+    else:
+        await bot.send(ev, '发生了未知错误,请联系管理员检查后台输出!')

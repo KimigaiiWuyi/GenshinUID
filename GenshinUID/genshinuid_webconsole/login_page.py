a6a842bf0e81ac6ce6b76f1f47c1e246f74e2418
KimgiaiiWuyi <444835641@qq.com>
2022-10-23 21:32:08 +0800

    ✨ 网页控制台为无管理员权限用户新增添加CK功能(#347)

--- GenshinUID/genshinuid_webconsole/login_page.py
+++ GenshinUID/genshinuid_webconsole/login_page.py
@@ -1,53 +1,105 @@
 from fastapi import Request
 from nonebot.typing import overrides
 import fastapi_amis_admin  # noqa: F401
+from starlette.requests import Request
+from fastapi_amis_admin.models.fields import Field
 from fastapi_user_auth import admin as user_auth_admin
 from fastapi_amis_admin.admin import admin as amis_admin
-from fastapi_amis_admin.amis.components import App, Tpl, Grid, Html, Page
+from fastapi_amis_admin.utils.translation import i18n as _
+from fastapi_amis_admin.amis.constants import LevelEnum, DisplayModeEnum
+from fastapi_amis_admin.amis.components import (
+    App,
+    Tpl,
+    Form,
+    Grid,
+    Html,
+    Page,
+    Action,
+    ActionType,
+    Horizontal,
+    ButtonToolbar,
+)
+
+from ..version import GenshinUID_version
 
 login_html = '''
-<p align="center">
-    <a href="https://github.com/KimigaiiWuyi/GenshinUID/">
-        <img src="https://s2.loli.net/2022/01/31/kwCIl3cF1Z2GxnR.png"
-         width="256" height="256" alt="GenshinUID">
+<p align='center'>
+    <a href='https://github.com/KimigaiiWuyi/GenshinUID/'>
+        <img src='https://s2.loli.net/2022/01/31/kwCIl3cF1Z2GxnR.png'
+         width='256' height='256' alt='GenshinUID'>
     </a>
 </p>
-<h1 align="center">GenshinUID 3.1 WebConsole</h1>
-<h4 align="center">
+<h1 align='center'>GenshinUID 3.1 WebConsole</h1>
+<h4 align='center'>
     ✨基于
-    <a href="https://github.com/Ice-Cirno/HoshinoBot" target="_blank">
+    <a href='https://github.com/Ice-Cirno/HoshinoBot' target='_blank'>
         HoshinoBot
     </a>
     /
-    <a href="https://github.com/nonebot/nonebot2" target="_blank">NoneBot2</a>
+    <a href='https://github.com/nonebot/nonebot2' target='_blank'>NoneBot2</a>
     /
-    <a href="https://bot.q.qq.com/wiki/#" target="_blank">QQ官方频道Bot</a>
+    <a href='https://bot.q.qq.com/wiki/#' target='_blank'>QQ官方频道Bot</a>
     的原神多功能插件✨
 </h4>
-<div align="center">
-    <a href="https://github.com/KimigaiiWuyi/GenshinUID/wiki" target="_blank">
+<div align='center'>
+    <a href='https://github.com/KimigaiiWuyi/GenshinUID/wiki' target='_blank'>
     安装文档</a> &nbsp; · &nbsp;
-    <a href="https://github.com/KimigaiiWuyi/GenshinUID/wiki/File5-「指令列表」"
-     target="_blank">指令列表</a> &nbsp; · &nbsp;
-    <a href="https://github.com/KimigaiiWuyi/GenshinUID/issues/226">常见问题</a>
+    <a href='https://github.com/KimigaiiWuyi/GenshinUID/wiki/File5-「指令列表」'
+     target='_blank'>指令列表</a> &nbsp; · &nbsp;
+    <a href='https://github.com/KimigaiiWuyi/GenshinUID/issues/226'>常见问题</a>
 </div>
 '''
 
-footer_html = '''
-<p align="right">
-    <div class="p-2 text-center bg-light">Copyright © 2021 - 2022
-        <a href="https://github.com/KimigaiiWuyi/GenshinUID" target="_blank"
-         class="link-secondary">GenshinUID 3.1
+footer_html = f'''
+<p align='right'>
+    <div class='p-2 text-center bg-light'>Copyright © 2021 - 2022
+        <a href='https://github.com/KimigaiiWuyi/GenshinUID' target='_blank'
+         class='link-secondary'>GenshinUID {GenshinUID_version}
         </a>
          X
-        <a target="_blank"
-         href="https://github.com/amisadmin/fastapi_amis_admin/"
-         class="link-secondary" rel="noopener">
-         fastapi_amis_admin 0.2.1
+        <a target='_blank'
+         href='https://github.com/amisadmin/fastapi_amis_admin/'
+         class='link-secondary' rel='noopener'>
+         fastapi_amis_admin {fastapi_amis_admin.__version__}
         </a>
     </div>
 </p>
 '''
+
+# 还原高版本的注册按钮丢失
+@overrides(user_auth_admin.UserLoginFormAdmin)
+async def get_form(self, request: Request) -> Form:
+    form = await super(user_auth_admin.UserLoginFormAdmin, self).get_form(
+        request
+    )
+    form.body.sort(key=lambda form_item: form_item.type, reverse=True)  # type: ignore
+    form.update_from_kwargs(
+        title='',
+        mode=DisplayModeEnum.horizontal,
+        submitText=_('Sign in'),
+        actionsClassName='no-border m-none p-none',
+        panelClassName='',
+        wrapWithPanel=True,
+        horizontal=Horizontal(left=3, right=9),
+        actions=[
+            ButtonToolbar(
+                buttons=[
+                    ActionType.Link(
+                        actionType='link',
+                        link=f'{self.router_path}/reg',
+                        label=_('Sign up'),
+                    ),
+                    Action(
+                        actionType='submit',
+                        label=_('Sign in'),
+                        level=LevelEnum.primary,
+                    ),
+                ]
+            )
+        ],
+    )
+    form.redirect = request.query_params.get('redirect') or '/genshinuid'
+    return form
 
 
 @overrides(user_auth_admin)
@@ -56,7 +108,7 @@ def attach_page_head(page: Page) -> Page:
         Html(html=login_html),
         Grid(
             columns=[
-                {"body": [page.body], "lg": 2, "md": 4, "valign": "middle"}
+                {'body': [page.body], 'lg': 3, 'md': 4, 'valign': 'middle'}
             ],
             align='center',
             valign='middle',
@@ -72,11 +124,11 @@ async def _get_page_as_app(self, request: Request) -> App:
     app.header = Tpl(
         className='w-full',
         tpl='''
-        <div class="flex justify-between">
+        <div class='flex justify-between'>
             <div>
-                <a href="https://github.com/KimigaiiWuyi/GenshinUID"
-                target="_blank" title="Copyright">
-                    <i class="fa fa-github fa-2x"></i>
+                <a href='https://github.com/KimigaiiWuyi/GenshinUID'
+                target='_blank' title='Copyright'>
+                    <i class='fa fa-github fa-2x'></i>
                 </a>
             </div>
         </div>
@@ -90,3 +142,4 @@ async def _get_page_as_app(self, request: Request) -> App:
 
 amis_admin.AdminApp._get_page_as_app = _get_page_as_app
 user_auth_admin.attach_page_head = attach_page_head
+user_auth_admin.UserLoginFormAdmin.get_form = get_form

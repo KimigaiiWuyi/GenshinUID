5f5617055da829fca4d495b5de3cbceedec20452
KimgiaiiWuyi <444835641@qq.com>
2022-10-23 21:41:01 +0800

    ✨ 网页控制台为无管理员权限用户新增添加CK功能(#347)

--- GenshinUID/genshinuid_webconsole/mount_app.py
+++ GenshinUID/genshinuid_webconsole/mount_app.py
@@ -1,33 +1,55 @@
 import platform
 from pathlib import Path
-from typing import Any, Set, Dict, Callable
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Set,
+    Dict,
+    List,
+    Type,
+    Union,
+    Callable,
+    Optional,
+    cast,
+)
 
 from quart import Quart
 from nonebot import get_bot
+from pydantic import BaseModel
+from sqlalchemy.orm import backref
 from fastapi import FastAPI, Request
 import fastapi_amis_admin  # noqa: F401
+from sqlmodel import Field, Relationship
 from fastapi_amis_admin import amis, admin
+from fastapi_amis_admin.crud import BaseApiOut
 from sqlalchemy.ext.asyncio import AsyncEngine
 from fastapi_user_auth.site import AuthAdminSite
 from fastapi_amis_admin.amis.types import AmisAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_amis_admin.crud.schema import CrudEnum
 from fastapi_amis_admin.admin.settings import Settings
-from fastapi_user_auth.auth.models import UserRoleLink
-from fastapi_amis_admin.amis.constants import DisplayModeEnum
+from fastapi_amis_admin.utils.translation import i18n as _
+from fastapi_user_auth.auth.models import User, UserRoleLink
 from fastapi_amis_admin.admin.site import DocsAdmin, ReDocsAdmin
+from fastapi_amis_admin.amis.constants import LevelEnum, DisplayModeEnum
 from fastapi_amis_admin.amis.components import (
     App,
     Form,
+    Grid,
     Page,
     Alert,
+    Action,
     Property,
+    ActionType,
+    Horizontal,
     InputExcel,
     InputTable,
     PageSchema,
+    ButtonToolbar,
 )
 
 from ..version import GenshinUID_version
+from ..genshinuid_user.add_ck import _deal_ck
 from .login_page import amis_admin, user_auth_admin  # 不要删!!
 from ..utils.db_operation.database.db_config import DATABASE_URL
 from ..utils.db_operation.database.models import (
@@ -48,9 +70,9 @@ class FastApiMiddleware:
     async def __call__(
         self, scope: Dict[str, Any], receive: Callable, send: Callable
     ) -> None:
-        if scope["type"] in ("http", "websocket"):
+        if scope['type'] in ('http', 'websocket'):
             for path in self.routes:
-                if scope["path"].startswith(path):
+                if scope['path'].startswith(path):
                     return await self.fastapi(scope, receive, send)
             return await self.app(scope, receive, send)
 
@@ -88,7 +110,7 @@ settings = Settings(
     language='zh_CN',
 )
 quart = get_bot().server_app
-quart.asgi_app = FastApiMiddleware(quart, app, {"/genshinuid"})  # type: ignore
+quart.asgi_app = FastApiMiddleware(quart, app, {'/genshinuid'})  # type: ignore
 
 
 # 显示主键
@@ -98,7 +120,7 @@ async def patched_get_create_form(
     fields = list(self.schema_create.__fields__.values())
     if not bulk:
         return Form(
-            api=f"post:{self.router_path}/item",
+            api=f'post:{self.router_path}/item',
             name=CrudEnum.create,
             body=await self._conv_modelfields_to_formitems(
                 request, fields, CrudEnum.create
@@ -109,20 +131,20 @@ async def patched_get_create_form(
         column = await self.get_list_column(
             request, self.parser.get_modelfield(field, deepcopy=True)
         )
-        keys[column.name] = "${" + column.label + "}"
+        keys[column.name] = '${' + column.label + '}'
         column.name = column.label
         columns.append(column)
     return Form(
         api=AmisAPI(
-            method="post",
-            url=f"{self.router_path}/item",
-            data={"&": {"$excel": keys}},
+            method='post',
+            url=f'{self.router_path}/item',
+            data={'&': {'$excel': keys}},
         ),
         mode=DisplayModeEnum.normal,
         body=[
-            InputExcel(name="excel"),
+            InputExcel(name='excel'),
             InputTable(
-                name="excel",
+                name='excel',
                 showIndex=True,
                 columns=columns,
                 addable=True,
@@ -136,10 +158,23 @@ async def patched_get_create_form(
 
 admin.BaseModelAdmin.get_create_form = patched_get_create_form
 
+
+class MyUser(User, table=True):
+    point: float = Field(default=0, title=_('Point'))
+    phone: str = Field(None, title=_('Tel'), max_length=15)  # type: ignore
+    parent_id: int = Field(None, title=_('Parent'), foreign_key='auth_user.id')  # type: ignore
+    children: List['User'] = Relationship(
+        sa_relationship_kwargs=dict(
+            backref=backref('parent', remote_side='User.id'),
+        ),
+    )
+
+
 # 创建AdminSite实例
 site = GenshinUIDAdminSite(settings)
 auth = site.auth
 config = get_bot().config
+auth.user_model = MyUser
 
 app.add_middleware(
     CORSMiddleware,
@@ -149,11 +184,145 @@ app.add_middleware(
     allow_headers=['*'],
 )
 
+gsuid_webconsole_help = '''
+## 初次使用
+欢迎进入网页控制台!
+Admin账户可以通过左侧的选项进入不同的数据库直接修改,**首次登陆的Admin账户别忘了修改你的密码!**
+普通账户可以通过左侧的选项进行绑定CK或者SK
+未来还会加入更多功能!
+## 获取CK通则
+**如果获取到的Cookies字段不全，无法通过校验**
+**推荐重新登陆米游社再进行获取**
+## 网页端 #92 [@RemKeeper](https://github.com/RemKeeper)
+[通过网页控制台简易获取Cookies](https://github.com/KimigaiiWuyi/GenshinUID/issues/92)
+## 安卓 [@shirokurakana](https://github.com/shirokurakana)
+[通过额外APP获取Cookies](https://github.com/KimigaiiWuyi/GenshinUID/issues/203)
+## IOS [@741807012](https://github.com/741807012)
+[通过快捷指令获取Cookies](https://github.com/KimigaiiWuyi/GenshinUID/issues/201)
+## 丨我该如何获取Cookies？[#92](https://github.com/KimigaiiWuyi/GenshinUID/issues/92)（[@RemKeeper](https://github.com/RemKeeper)）
+```js
+var cookie = document.cookie;
+var Str_Num = cookie.indexOf('_MHYUUID=');
+cookie = '添加 ' + cookie.substring(Str_Num);
+var ask = confirm('Cookie:' + cookie + '\n\n按确认，然后粘贴发送给机器人');
+if (ask == true) {
+  copy(cookie);
+  msg = cookie
+} else {
+  msg = 'Cancel'
+}
+```
+1. 复制上面全部代码，然后打开[米游社BBS](https://bbs.mihoyo.com/ys/)
+2. 在页面上右键检查或者Ctrl+Shift+i
+3. 选择控制台（Console），粘贴，回车，在弹出的窗口点确认（点完自动复制）
+4. 然后在和机器人的私聊窗口，粘贴发送即可
+**警告：Cookies属于个人隐私，其效用相当于账号密码，请勿随意公开！**
+## 丨获取米游社Stoken([AutoMihoyoBBS](https://github.com/Womsxd/AutoMihoyoBBS#%E8%8E%B7%E5%8F%96%E7%B1%B3%E6%B8%B8%E7%A4%BECookie))
+```js
+var cookie = document.cookie;
+var ask = confirm('Cookie:' + cookie + '\n\nDo you want to copy the cookie to the clipboard?');
+if (ask == true) {
+  copy('添加 stoken' + cookie);
+  msg = cookie
+} else {
+  msg = 'Cancel'
+}
+```
+1. 复制上面全部代码，然后打开[米游社账户登录界面](http://user.mihoyo.com/)
+2. 在页面上右键检查或者Ctrl+Shift+i
+3. 选择控制台（Console），粘贴，回车，在弹出的窗口点确认（点完自动复制）
+4. 然后在和机器人的私聊窗口，粘贴发送即可
+**警告：Cookies属于个人隐私，其效用相当于账号密码，请勿随意公开！**
+'''
+
+
+@site.register_admin
+class AmisPageAdmin(admin.PageAdmin):
+    page_schema = '入门使用'
+    page = Page.parse_obj(
+        {
+            'type': 'page',
+            'body': {
+                'type': 'markdown',
+                'value': f'{gsuid_webconsole_help}',
+            },
+        }
+    )
+
+
+@site.register_admin
+class UserBindFormAdmin(admin.FormAdmin):
+    page_schema = PageSchema(label='绑定CK或SK', icon='fa fa-link')  # type: ignore
+
+    async def get_form(self, request: Request) -> Form:
+        form = await super().get_form(request)
+        form.body.sort(key=lambda form_item: form_item.type, reverse=True)  # type: ignore
+        form.update_from_kwargs(
+            title='',
+            mode=DisplayModeEnum.horizontal,
+            submitText='绑定',
+            actionsClassName='no-border m-none p-none',
+            panelClassName='',
+            wrapWithPanel=True,
+            horizontal=Horizontal(left=3, right=9),
+            actions=[
+                ButtonToolbar(
+                    buttons=[
+                        Action(
+                            actionType='submit',
+                            label='绑定',
+                            level=LevelEnum.primary,
+                        )
+                    ]
+                )
+            ],
+        )
+        return form
+
+    async def get_page(self, request: Request) -> Page:
+        page = await super().get_page(request)
+        page.body = [
+            Alert(
+                level='warning',
+                body='CK获取可查看左侧栏 [入门使用] 相关细则!',
+            ),
+            amis.Divider(),
+            Grid(
+                columns=[
+                    {
+                        'body': [page.body],
+                        'lg': 10,
+                        'md': 10,
+                        'valign': 'middle',
+                    }
+                ],
+                align='center',
+                valign='middle',
+            ),
+        ]
+        return page
+
+    # 创建表单数据模型
+    class schema(BaseModel):
+        QQ: str = Field(..., title='QQ号', min_length=3, max_length=30)
+        Cookies: str = Field(..., title='Cookies或者Login_ticket')
+
+    # 处理表单提交数据
+    async def handle(
+        self, request: Request, data: schema, **kwargs
+    ) -> BaseApiOut[Any]:
+        im = await _deal_ck(data.Cookies, data.QQ)
+        ok_num = im.count('成功')
+        if ok_num < 1:
+            return BaseApiOut(status=-1, msg=im)
+        else:
+            return BaseApiOut(msg=im)
+
 
 @site.register_admin
 class UserAuth(admin.ModelAdmin):
     pk_name = 'user_id'
-    page_schema = '用户授权'
+    page_schema = PageSchema(label='用户授权', icon='fa fa-user-o')  # type: ignore
 
     # 配置管理模型
     model = UserRoleLink
@@ -165,7 +334,7 @@ class UserAuth(admin.ModelAdmin):
 @site.register_admin
 class CKadmin(admin.ModelAdmin):
     pk_name = 'UID'
-    page_schema = 'CK管理'
+    page_schema = PageSchema(label='CK管理', icon='fa fa-database')  # type: ignore
 
     # 配置管理模型
     model = NewCookiesTable
@@ -177,7 +346,7 @@ class CKadmin(admin.ModelAdmin):
 @site.register_admin
 class Configadmin(admin.ModelAdmin):
     pk_name = 'Name'
-    page_schema = '配置项管理'
+    page_schema = PageSchema(label='配置项管理', icon='fa fa-toggle-on')  # type: ignore
 
     # 配置管理模型
     model = Config
@@ -189,7 +358,7 @@ class Configadmin(admin.ModelAdmin):
 @site.register_admin
 class pushadmin(admin.ModelAdmin):
     pk_name = 'UID'
-    page_schema = '推送管理'
+    page_schema = PageSchema(label='推送管理', icon='fa fa-bullhorn')  # type: ignore
 
     # 配置管理模型
     model = PushData
@@ -201,7 +370,7 @@ class pushadmin(admin.ModelAdmin):
 @site.register_admin
 class cacheadmin(admin.ModelAdmin):
     pk_name = 'UID'
-    page_schema = '缓存管理'
+    page_schema = PageSchema(label='缓存管理', icon='fa fa-recycle')  # type: ignore
 
     # 配置管理模型
     model = CookiesCache
@@ -213,7 +382,7 @@ class cacheadmin(admin.ModelAdmin):
 @site.register_admin
 class bindadmin(admin.ModelAdmin):
     pk_name = 'USERID'
-    page_schema = '绑定管理'
+    page_schema = PageSchema(label='绑定管理', icon='fa fa-users')  # type: ignore
 
     # 配置管理模型
     model = UidData
@@ -226,9 +395,7 @@ class bindadmin(admin.ModelAdmin):
 @site.register_admin
 class GitHubIframeAdmin(admin.IframeAdmin):
     # 设置页面菜单信息
-    page_schema = PageSchema(  # type: ignore
-        label='安装Wiki', icon='fa fa-github'
-    )
+    page_schema = PageSchema(label='安装Wiki', icon='fa fa-github')  # type: ignore
     # 设置跳转链接
     src = 'https://github.com/KimigaiiWuyi/GenshinUID/wiki'
 

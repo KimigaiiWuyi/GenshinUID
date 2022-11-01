3b543f9beda2cb0ff870d88a1e38a1ad04814d0c
KimgiaiiWuyi <444835641@qq.com>
2022-11-01 22:55:38 +0800

    🎨 统一角色面板处理文件

--- GenshinUID/genshinuid_enka/__init__.py
+++ GenshinUID/genshinuid_enka/__init__.py
@@ -1,5 +1,4 @@
 import re
-import json
 import random
 import asyncio
 from typing import Tuple
@@ -12,20 +11,19 @@ from nonebot.params import Depends, CommandArg
 from nonebot.adapters.qqguild import Bot, Message, MessageEvent
 
 from ..config import priority
-from .draw_char_card import draw_char_img
+from .get_enka_img import draw_enka_img
 from ..genshinuid_meta import register_menu
 from ..utils.nonebot2.send import local_image
 from .draw_char_rank import draw_cahrcard_list
+from ..utils.message.error_reply import UID_HINT
 from ..utils.message.cast_type import cast_to_int
 from ..utils.enka_api.get_enka_data import switch_api
+from ..utils.enka_api.enka_to_card import enka_to_card
 from ..utils.enka_api.enka_to_data import enka_to_data
 from ..utils.message.get_image_and_at import ImageAndAt
-from ..utils.message.error_reply import UID_HINT, CHAR_HINT
-from ..utils.alias.alias_to_char_name import alias_to_char_name
+from ..utils.download_resource.RESOURCE_PATH import TEMP_PATH
 from ..utils.exception.handle_exception import handle_exception
 from ..utils.db_operation.db_operation import select_db, get_all_uid
-from ..utils.enka_api.enka_to_card import enka_to_card, draw_enka_card
-from ..utils.download_resource.RESOURCE_PATH import TEMP_PATH, PLAYER_PATH
 
 refresh = on_command('强制刷新')
 original_pic = on_command('原图')
@@ -36,16 +34,6 @@ get_char_info = on_command(
     priority=priority,
 )
 
-CONVERT_TO_INT = {
-    '零': 0,
-    '一': 1,
-    '二': 2,
-    '三': 3,
-    '四': 4,
-    '五': 5,
-    '六': 6,
-    '满': 6,
-}
 AUTO_REFRESH = False
 refresh_scheduler = require('nonebot_plugin_apscheduler').scheduler
 
@@ -135,55 +123,7 @@ async def send_char_info(
 
     # 获取角色名
     msg = ''.join(re.findall('[\u4e00-\u9fa5]', raw_mes))
-    is_curve = False
-    if '成长曲线' in msg or '曲线' in msg:
-        is_curve = True
-        msg = msg.replace('成长曲线', '').replace('曲线', '')
-    msg_list = msg.split('换')
-    char_name = msg_list[0]
-    talent_num = None
-    weapon = None
-    weapon_affix = None
-    if '命' in char_name and char_name[0] in CONVERT_TO_INT:
-        talent_num = CONVERT_TO_INT[char_name[0]]
-        char_name = char_name[2:]
-    if len(msg_list) > 1:
-        if (
-            '精' in msg_list[1]
-            and msg_list[1][1] != '六'
-            and msg_list[1][1] != '零'
-            and msg_list[1][1] in CONVERT_TO_INT
-        ):
-            weapon_affix = CONVERT_TO_INT[msg_list[1][1]]
-            weapon = msg_list[1][2:]
-        else:
-            weapon = msg_list[1]
-
-    player_path = PLAYER_PATH / str(uid)
-    if char_name == '展柜角色':
-        char_file_list = player_path.glob('*')
-        char_list = []
-        for i in char_file_list:
-            file_name = i.name
-            if '\u4e00' <= file_name[0] <= '\u9fff':
-                char_list.append(file_name.split('.')[0])
-        img = await draw_enka_card(uid=uid, char_list=char_list)
-        await matcher.finish(local_image(img))
-    else:
-        if '旅行者' in char_name:
-            char_name = '旅行者'
-        else:
-            char_name = await alias_to_char_name(char_name)
-        char_path = player_path / f'{char_name}.json'
-        if char_path.exists():
-            with open(char_path, 'r', encoding='utf8') as fp:
-                char_data = json.load(fp)
-        else:
-            await matcher.finish(CHAR_HINT.format(char_name), at_sender=True)
-
-    im = await draw_char_img(
-        char_data, weapon, weapon_affix, talent_num, img, is_curve
-    )
+    im = await draw_enka_img(msg, uid, img)
 
     if isinstance(im, str):
         await matcher.finish(im)

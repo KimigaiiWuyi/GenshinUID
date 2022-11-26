1ec87752a48f60a14e96c77cbdfc20d4d6e6b1f1
KimgiaiiWuyi <444835641@qq.com>
2022-11-26 23:31:42 +0800

    🚑️ 补充错误获取的CK

--- GenshinUID/genshinuid_enka/mono/SEQ.py
+++ GenshinUID/genshinuid_enka/mono/SEQ.py
@@ -1,22 +1,28 @@
-TEST_SEQ = [
-    {'char': '达达利亚', 'action': 'A满蓄力瞄准射击', 'type': '伤害'},
-    {'char': '枫原万叶', 'action': 'E长按伤害', 'type': '伤害'},
-    {'char': '枫原万叶', 'action': 'Q斩击伤害', 'type': '伤害'},
-    {'char': '枫原万叶', 'action': 'Q持续伤害', 'type': '持续'},
-    {'char': '班尼特', 'action': 'E点按伤害', 'type': '伤害'},
-    {'char': '班尼特', 'action': 'Q伤害', 'type': '伤害'},
-    {'char': '香菱', 'action': 'E喷火伤害', 'type': '持续'},
-    {'char': '香菱', 'action': 'Q旋火轮伤害', 'type': '持续'},
+WAN_DA = [
     {'char': '达达利亚', 'action': 'E状态激发伤害', 'type': '伤害'},
     {'char': '达达利亚', 'action': 'E一段伤害', 'type': '伤害'},
     {'char': '达达利亚', 'action': 'E重击伤害', 'type': '伤害'},
+    {'char': '班尼特', 'action': 'Q伤害', 'type': '伤害'},
+    {'char': '枫原万叶', 'action': 'Q斩击伤害', 'type': '伤害'},
+    {'char': '枫原万叶', 'action': '扩散', 'type': '持续'},
+    {'char': '枫原万叶', 'action': 'E长按伤害', 'type': '伤害'},
+    {'char': '枫原万叶', 'action': 'Q持续伤害', 'type': '持续'},
+    {'char': '枫原万叶', 'action': 'A下落伤害', 'type': '伤害'},
+    {'char': '香菱', 'action': 'Q旋火轮伤害', 'type': '持续'},
+    {'char': '香菱', 'action': 'E喷火伤害', 'type': '持续'},
+    {'char': '达达利亚', 'action': 'E状态激发伤害', 'type': '伤害'},
+    {'char': '枫原万叶', 'action': 'Q持续伤害', 'type': '持续'},
     {'char': '达达利亚', 'action': 'E一段伤害', 'type': '伤害'},
-    {'char': '达达利亚', 'action': 'E重击伤害', 'type': '伤害'},
-    {'char': '达达利亚', 'action': 'E一段伤害', 'type': '伤害'},
-    {'char': '达达利亚', 'action': 'E重击伤害', 'type': '伤害'},
     {'char': '达达利亚', 'action': 'Q伤害·近战', 'type': '伤害'},
+    {'char': '达达利亚', 'action': 'E一段伤害', 'type': '伤害'},
+    {'char': '达达利亚', 'action': 'E重击伤害', 'type': '伤害'},
+    {'char': '达达利亚', 'action': 'E一段伤害', 'type': '伤害'},
+    {'char': '枫原万叶', 'action': 'Q持续伤害', 'type': '持续'},
+    {'char': '达达利亚', 'action': 'E重击伤害', 'type': '伤害'},
+    {'char': '达达利亚', 'action': 'E一段伤害', 'type': '伤害'},
+    {'char': '达达利亚', 'action': 'E重击伤害', 'type': '伤害'},
 ]
 
-ALL_SEQ = {'万达国际': TEST_SEQ}
+ALL_SEQ = {'万达国际': WAN_DA}
 
 SEQ_ARG = {'万达国际': ['香菱', '达达利亚', '班尼特', '枫原万叶']}

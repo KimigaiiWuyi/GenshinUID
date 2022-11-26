1f34c9d8a2f5be408343ba2ad6f5ede68cbb1a4e
KimgiaiiWuyi <444835641@qq.com>
2022-11-26 15:15:28 +0800

    ✨ 新增`深渊统计`(`深渊概览`)

---
+++ GenshinUID/genshinuid_xkdata/__init__.py
@@ -0,0 +1,26 @@
+import random
+import asyncio
+import threading
+
+from nonebot.log import logger
+
+from ..all_import import *  # noqa: F403,F401
+from .draw_abyss_total import TOTAL_IMG, draw_xk_abyss_img
+
+
+@sv.scheduled_job('interval', hours=3)
+async def scheduled_draw_abyss():
+    await asyncio.sleep(random.randint(0, 60))
+    await draw_xk_abyss_img()
+
+
+@sv.on_fullmatch(('深渊概览', '深渊统计', '深渊使用率'))
+async def send_abyss_pic(bot, ev):
+    img = await convert_img(TOTAL_IMG)
+    logger.info('获得gs帮助图片成功！')
+    await bot.send(ev, img)
+
+
+threading.Thread(
+    target=lambda: asyncio.run(draw_xk_abyss_img()), daemon=True
+).start()

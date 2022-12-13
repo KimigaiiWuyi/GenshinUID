a1f086d24bda0b8cdfd895784f9537ad6ec48b75
KimigaiiWuyi <444835641@qq.com>
2022-12-13 23:32:21 +0800

    :zap: `攻略`换用本地攻略源

--- GenshinUID/genshinuid_guide/__init__.py
+++ GenshinUID/genshinuid_guide/__init__.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 
-import httpx
-
 from ..all_import import *
 from .get_card import get_gs_card
+from .get_guide import get_gs_guide
 from ..utils.alias.alias_to_char_name import alias_to_char_name
 
 IMG_PATH = Path(__file__).parent / 'img'
@@ -11,18 +10,22 @@ IMG_PATH = Path(__file__).parent / 'img'
 
 @sv.on_rex('([\u4e00-\u9fa5]+)(推荐|攻略)')
 async def send_guide_pic(bot: HoshinoBot, ev: CQEvent):
-    name = str(ev['match'].group(1))
-    if not name:
-        return
-    name = await alias_to_char_name(name)
-    if name.startswith('旅行者'):
-        name = f'{name[:3]}-{name[-1]}'
-    url = 'https://file.microgg.cn/MiniGG/guide/{}.jpg'.format(name)
-    if httpx.head(url).status_code == 200:
-        logger.info('获得{}推荐图片成功！'.format(name))
-        await bot.send(ev, MessageSegment.image(url))
+    if ev.message:
+        name = name = str(ev['match'].group(1))
     else:
-        logger.warning('未获得{}推荐图片。'.format(name))
+        return
+
+    if name == '':
+        return
+
+    im = await get_gs_guide(name)
+
+    if im:
+        logger.info('获得{}攻略成功！'.format(name))
+        im = await convert_img(im)
+        await bot.send(ev, im)
+    else:
+        logger.warning('未找到{}攻略图片'.format(name))
 
 
 @sv.on_prefix('参考面板')

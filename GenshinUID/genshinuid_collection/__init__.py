f361f0067a44b1b946e6688f4aa1b9974f933fa4
KimigaiiWuyi <444835641@qq.com>
2022-12-18 19:54:50 +0800

    ✨ 新增`查询探索`, 重绘的`查询收集`

--- GenshinUID/genshinuid_collection/__init__.py
+++ GenshinUID/genshinuid_collection/__init__.py
@@ -1,22 +1,14 @@
 from ..all_import import *  # noqa: F403,F401
-from .draw_collection_card import draw_collection_img
-from ..utils.db_operation.db_operation import select_db
-from ..utils.message.get_image_and_at import ImageAndAt
+from ..utils.data_convert.get_uid import get_uid
 from ..utils.message.error_reply import *  # noqa: F403,F401
-from ..utils.mhy_api.convert_mysid_to_uid import convert_mysid
+from .draw_collection_card import draw_explora_img, draw_collection_img
 
 
-@sv.on_rex(
-    r'^(\[CQ:at,qq=[0-9]+\])?( )?'
-    r'(uid|查询|mys)?([0-9]+)?'
-    r'(收集|宝箱|sj|bx)'
-    r'(\[CQ:at,qq=[0-9]+\])?( )?$',
-)
+@sv.on_prefix(('查询收集', '收集', 'sj'))
 async def send_collection_info(bot: HoshinoBot, ev: CQEvent):
-    args = ev['match'].groups()
+    raw_mes = ev.message.extract_plain_text().replace(' ', '')
     logger.info('开始执行[查询收集信息]')
-    logger.info('[查询收集信息]参数: {}'.format(args))
-    at = re.search(r'\[CQ:at,qq=(\d*)]', str(ev.message))
+    at = re.search(r'\[CQ:at,qq=(\d*)]', raw_mes)
 
     if at:
         qid = int(at.group(1))
@@ -26,22 +18,43 @@ async def send_collection_info(bot: HoshinoBot, ev: CQEvent):
         else:
             return
 
-    if args[2] != 'mys':
-        if args[3] is None:
-            uid = await select_db(qid, mode='uid')
-            uid = str(uid)
-        elif len(args[3]) != 9:
-            return
-        else:
-            uid = args[3]
-    else:
-        uid = await convert_mysid(args[3])
+    uid = await get_uid(qid, raw_mes)
     logger.info('[查询收集信息]uid: {}'.format(uid))
 
     if '未找到绑定的UID' in uid:
         await bot.send(ev, UID_HINT)
 
-    im = await draw_collection_img(uid)
+    im = await draw_collection_img(qid, uid)
+    if isinstance(im, str):
+        await bot.send(ev, im)
+    elif isinstance(im, bytes):
+        im = await convert_img(im)
+        await bot.send(ev, im)
+    else:
+        await bot.send(ev, '发生了未知错误,请联系管理员检查后台输出!')
+
+
+@sv.on_prefix(('查询探索', '探索', 'ts'))
+async def send_explora_info(bot: HoshinoBot, ev: CQEvent):
+    raw_mes = ev.message.extract_plain_text().replace(' ', '')
+    logger.info('开始执行[查询探索信息]')
+    at = re.search(r'\[CQ:at,qq=(\d*)]', raw_mes)
+
+    if at:
+        qid = int(at.group(1))
+    else:
+        if ev.sender:
+            qid = int(ev.sender['user_id'])
+        else:
+            return
+
+    uid = await get_uid(qid, raw_mes)
+    logger.info('[查询探索信息]uid: {}'.format(uid))
+
+    if '未找到绑定的UID' in uid:
+        await bot.send(ev, UID_HINT)
+
+    im = await draw_explora_img(qid, uid)
     if isinstance(im, str):
         await bot.send(ev, im)
     elif isinstance(im, bytes):

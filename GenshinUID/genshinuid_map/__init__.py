12d3d27f6562f02d2dbeff2b5a9b488cae68d7b0
KimigaiiWuyi <444835641@qq.com>
2023-01-15 17:42:48 +0800

    🐛 修复`xx在哪`的一部分使用体验 (#427)

--- GenshinUID/genshinuid_map/__init__.py
+++ GenshinUID/genshinuid_map/__init__.py
@@ -1,13 +1,19 @@
-from typing import Any, Dict
+from typing import Any, Dict, Union
 
 from nonebot.log import logger
 from nonebot.matcher import Matcher
 from nonebot.params import RegexDict
 from nonebot import on_regex, on_command
 from nonebot.permission import SUPERUSER
-from nonebot.adapters.qqguild import Bot, MessageEvent, MessageSegment
+from nonebot.adapters.onebot.v11 import (
+    Bot,
+    MessageSegment,
+    GroupMessageEvent,
+    PrivateMessageEvent,
+)
 
 from ..config import priority
+from ..genshinuid_meta import register_menu
 from ..utils.nonebot2.rule import FullCommand
 from .draw_genshinmap_card import MAP_DATA, draw_genshin_map
 from ..utils.exception.handle_exception import handle_exception
@@ -17,6 +23,7 @@ find_map = on_regex(
     r'^(?P<name>.*)(在哪里|在哪|哪里有|哪儿有|哪有|在哪儿)$', priority=priority
 )
 find_map2 = on_regex(r'^(哪里有|哪儿有|哪有)(?P<name>.*)$', priority=priority)
+
 
 MAP_ID_LIST = [
     '2',  # 提瓦特
@@ -34,9 +41,26 @@ MAP_CHN_NAME = {
 
 @change_map.handle()
 @handle_exception('切换地图')
+@register_menu(
+    '切换地图',
+    '切换地图',
+    '切换查找资源点功能所使用的地图',
+    detail_des=(
+        '介绍：\n'
+        '切换查找资源点功能所使用的地图\n'
+        '指令按列表顺序轮流切换地图\n'
+        '目前可用地图：'
+        + "；".join(
+            [f"<ft color=(238,120,0)>{x}</ft>" for x in MAP_CHN_NAME.values()]
+        )
+        + ' \n\n'
+        '指令：\n'
+        '- <ft color=(238,120,0)>切换地图</ft>'
+    ),
+)
 async def send_change_map_msg(
     bot: Bot,
-    event: MessageEvent,
+    event: Union[GroupMessageEvent, PrivateMessageEvent],
     matcher: Matcher,
 ):
     if not await SUPERUSER(bot, event):
@@ -53,6 +77,26 @@ async def send_change_map_msg(
 @find_map.handle()
 @find_map2.handle()
 @handle_exception('查找资源点')
+@register_menu(
+    '查找资源点',
+    'xx在哪',
+    '查找指定资源在地图上的位置',
+    detail_des=(
+        '介绍：\n'
+        '在米游社大地图上查询某资源的位置\n'
+        '使用 <ft color=(238,120,0)>切换地图</ft> 指令来切换目标地图'
+        ' \n'
+        '指令：\n'
+        '- <ft color=(0,148,200)>[资源名称]</ft>'
+        '<ft color=(238,120,0)>{在哪里|在哪|哪里有|哪儿有|哪有|在哪儿}</ft>\n'
+        '- <ft color=(238,120,0)>{哪里有|哪儿有|哪有}</ft>'
+        '<ft color=(0,148,200)>[资源名称]</ft>\n'
+        ' \n'
+        '示例：\n'
+        '- <ft color=(238,120,0)>甜甜花在哪</ft>\n'
+        '- <ft color=(238,120,0)>哪有清心</ft>'
+    ),
+)
 async def send_find_map_msg(
     matcher: Matcher, args: Dict[str, Any] = RegexDict()
 ):
@@ -61,9 +105,7 @@ async def send_find_map_msg(
     logger.info(f'[查找资源点]正在执行...当前地图为{map_name}')
     logger.info('[查找资源点]参数: {}'.format(args))
 
-    if args:
-        name = args.get('name', '')
-    else:
+    if not (args and (name := args.get('name'))):
         return
 
     if not MAP_DATA.exists():
@@ -72,14 +114,23 @@ async def send_find_map_msg(
     resource_temp_path = MAP_DATA / f'{map_name}_{name}.jpg'
     if resource_temp_path.exists():
         logger.info(f'本地已有{map_name}_{name}的资源点,直接发送...')
-        await matcher.finish(MessageSegment.file_image(resource_temp_path))
+        with open(resource_temp_path, 'rb') as f:
+            await matcher.finish(MessageSegment.image(f.read()))
     else:
-        await matcher.send((f'正在查找{name},可能需要比较久的时间...\n' f'当前地图：{map_name}'))
+        # 放弃安慰剂回复
+        '''
+        await matcher.send(
+            (
+                f'正在查找{name},可能需要比较久的时间...\n'
+                f'当前地图：{MAP_CHN_NAME.get(MAP_ID_LIST[0])}'
+            )
+        )
+        '''
         logger.info('本地未缓存,正在渲染...')
         im = await draw_genshin_map(name, map_id, map_name)
-        if isinstance(im, str):
-            await matcher.finish(im)
-        elif isinstance(im, bytes):
-            await matcher.finish(MessageSegment.file_image(im))
-        else:
-            await matcher.finish('查找失败!')
+    if isinstance(im, str):
+        await matcher.finish()
+    elif isinstance(im, bytes):
+        await matcher.finish(MessageSegment.image(im))
+    else:
+        await matcher.finish('查找失败!')

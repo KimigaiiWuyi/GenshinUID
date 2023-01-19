b9519acf89337785890c3ce4b2342bb862f2a90c
KimigaiiWuyi <444835641@qq.com>
2023-01-19 23:19:37 +0800

    🍱 支持部分`3.5版本`的角色和武器的替换

--- GenshinUID/tools/download_icon.py
+++ GenshinUID/tools/download_icon.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import httpx
 
-char_list = ['Dori']
+char_list = ['Dehya', 'Mika']
 title = 'https://api.ambr.top/assets/UI/{}'
 # title = 'https://enka.network/ui/{}'
 icon_list = [

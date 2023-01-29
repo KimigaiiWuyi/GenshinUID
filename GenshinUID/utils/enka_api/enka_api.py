ecb286b33f137e7bf6619f7e2c1bd3fb24b19f46
KimigaiiWuyi <444835641@qq.com>
2023-01-29 22:52:15 +0800

    :alien: 更新`ENKA_API`

--- GenshinUID/utils/enka_api/enka_api.py
+++ GenshinUID/utils/enka_api/enka_api.py
@@ -1,3 +1,3 @@
-ENKA_DATA_URL = 'https://enka.network/u/{}/__data.json'
+ENKA_DATA_URL = 'https://enka.network/api/uid/{}'
 MICROGG_DATA_URL = 'https://enka.microgg.cn/u/{}/__data.json'
 MINIGG_DATA_URL = 'https://enka.minigg.cn/u/{}/__data.json'

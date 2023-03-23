a8b097e9907628769842d0685e28f0904c6f34e0
Wuyi无疑 <444835641@qq.com>
2023-03-24 00:53:04 +0800

    🐛 修复启动错误

--- GenshinUID/genshinuid_start/__init__.py
+++ GenshinUID/genshinuid_start/__init__.py
@@ -1,4 +1,7 @@
-from gsuid_core.gss import gss
+import asyncio
+import threading
+
+from gsuid_core.logger import logger
 
 from ..genshinuid_resource import startup
 from ..genshinuid_xkdata import draw_xk_abyss_img
@@ -6,9 +9,14 @@ from ..genshinuid_help.draw_help_card import draw_help_img
 from ..genshinuid_guide.get_abyss_data import generate_data
 
 
-@gss.on_bot_connect
 async def all_start():
-    await startup()
-    await draw_xk_abyss_img()
-    await draw_help_img()
-    await generate_data()
+    try:
+        await startup()
+        await draw_xk_abyss_img()
+        await draw_help_img()
+        await generate_data()
+    except Exception as e:
+        logger.exception(e)
+
+
+threading.Thread(target=lambda: asyncio.run(all_start()), daemon=True).start()

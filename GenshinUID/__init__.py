6cbe65ffff4a267e5d333c9ea73e6d5ca15cb13e
wangyu1997 <wangyu19970819@qq.com>
2023-04-04 23:59:43 +0800

    修复wechat session分割导致的userid不完整问题

--- GenshinUID/__init__.py
+++ GenshinUID/__init__.py
@@ -89,8 +89,8 @@ async def send_char_adv(bot: Bot, ev: Event):
         return await connect()
 
     # 通用字段获取
-    ev_session = ev.get_session_id().replace('wxid_','%')
-    sessions = [i.replace('%','wxid_') for i in ev_session.split('_')]
+    pattern = r'(?<!wxid)_'
+    sessions = re.split(pattern, ev.get_session_id())
     user_id = str(ev.get_user_id())
     messages = ev.get_message()
     raw_data = ev.__dict__

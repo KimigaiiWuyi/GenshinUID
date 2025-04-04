20bbd7871209babfe3965161185325d0b459fb4a
Autuamn_End <autuamn@qq.com>
2025-04-04 23:16:03 +0800

    🐛 不支持直接传入 Attachment，转为 dict
    🐛 其它类文件 `type='attachment'`

--- GenshinUID/__init__.py
+++ GenshinUID/__init__.py
@@ -671,15 +671,21 @@ async def get_all_message(bot: Bot, ev: Event):
         # 处理 Discord 消息中的附件
         if ev.attachments:
             from nonebot.adapters.discord.api import UNSET
+            from nonebot.adapters.discord.utils import model_dump
 
-            for dc_attachment in ev.attachments:
-                if (content_type := dc_attachment.content_type) is not UNSET:
-                    message.append(
-                        Message(
-                            'image' if 'image' in content_type else 'file',
-                            dc_attachment,
-                        )
-                    )
+            message.extend(
+                Message(
+                    (
+                        'image'
+                        if (content_type := dc_attachment.content_type)
+                        is not UNSET
+                        and 'image' in content_type
+                        else 'attachment'
+                    ),
+                    model_dump(dc_attachment, exclude_unset=True),
+                )
+                for dc_attachment in ev.attachments
+            )
     elif bot.adapter.get_name() == 'DoDo':
         from nonebot.adapters.dodo import (
             ChannelMessageEvent,

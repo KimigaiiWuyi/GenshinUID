6ab04450a98555744a83cee95d5cc36a0d34df28
KimigaiiWuyi <444835641@qq.com>
2025-07-25 17:43:58 +0800

    🔥 移除多余导入

--- GenshinUID/genshinuid_hard_challenge/__init__.py
+++ GenshinUID/genshinuid_hard_challenge/__init__.py
@@ -1,9 +1,9 @@
 import re
 
-from loguru import logger
 from gsuid_core.sv import SV
 from gsuid_core.bot import Bot
 from gsuid_core.models import Event
+from gsuid_core.logger import logger
 
 from ..utils.convert import get_uid
 from ..utils.message import UID_HINT
